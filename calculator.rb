e0405c26cdfdef8ffac4aab4365a293ca7759951
J <jmrumingan@gmail.com>
2019-10-12 12:58:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,8 @@
 # Add your variables here
 first_number = 5
+second_number = 9
+
+sum = first_number + second_number
+difference = first_number - second first_number
+product = first_number*second_number
+difference = first_number/second_number
