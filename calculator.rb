3419dcadcb67f7617e27a6add8eeabdb119fb3db
J <jmrumingan@gmail.com>
2019-10-12 13:00:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,6 +3,6 @@ first_number = 5.0
 second_number = 9
 
 sum = first_number + second_number
-difference = first_number - second_number
+difference = first_number-second_number
 product = first_number*second_number
 difference = first_number/second_number
